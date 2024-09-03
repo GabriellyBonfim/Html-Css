059d78d754dee0c9be25867327992ab86f73042a
GabriellyBonfim <gabriellybonfim03@gmail.com>
2024-09-03 12:00:35 -0300

    ll
    jjj

---
+++ Javascript/1exscript.js
@@ -0,0 +1,14 @@
+function entrar() {
+    let a = document.getElementById('itens')
+    a.style.background = 'white'
+}
+
+function sair() {
+    let a = document.getElementsByClassName('itens')
+    a.style.background = 'black'
+}
+
+function clicar(){
+    let a = document.getElementsByClassName('itens')
+    a.style.background = 'red'
+}
