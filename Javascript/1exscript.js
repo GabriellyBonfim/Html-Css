e5485f44e5cdb3120c64f4733db0a41ea33e3e9e
GabriellyBonfim <gabriellybonfim03@gmail.com>
2024-09-05 12:00:33 -0300

    ll
    ff

--- Javascript/1exscript.js
+++ Javascript/1exscript.js
@@ -1,14 +1,41 @@
-function entrar() {
-    let a = document.getElementById('itens')
-    a.style.background = 'white'
+let a = document.getElementsByClassName('itens')[0]
+let b = document.getElementsByClassName('itens')[1]
+let c = document.getElementsByClassName('itens')[2]
+let d = document.getElementsByClassName('itens')[3]
+let e = document.getElementsByClassName('itens')[4]
+let f = document.getElementsByClassName('itens')[5]
+
+function entrar1() {
+    a.style.backgroundColor = 'white'
 }
+
+function entrar2() {
+    b.style.backgroundColor = 'white'
+}
+
+function entrar3() {
+    c.style.backgroundColor = 'white'
+}
+
+function entrar4() {
+    d.style.backgroundColor = 'white'
+}
+
+function entrar5() {
+    e.style.backgroundColor = 'white'
+}
+
+function entrar6() {
+    f.style.backgroundColor = 'white'
+}
+
 
 function sair() {
-    let a = document.getElementsByClassName('itens')
-    a.style.background = 'black'
+    a.style.backgroundColor = 'black'
+    b.style.backgroundColor = 'black'    
+    c.style.backgroundColor = 'black'
+    d.style.backgroundColor = 'black'
+    e.style.backgroundColor = 'black'
+    f.style.backgroundColor = 'black'
 }
 
-function clicar(){
-    let a = document.getElementsByClassName('itens')
-    a.style.background = 'red'
-}
