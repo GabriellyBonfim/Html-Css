04a936618ad80882722ce0df62400c33d185d692
GabriellyBonfim <gabriellybonfim03@gmail.com>
2024-10-02 12:04:10 -0300

    aa
    aaa

--- Javascript/exe2.js
+++ Javascript/exe2.js
@@ -1,19 +1,17 @@
-// Obtém os elementos do DOM
+
 const modal = document.getElementById("modal");
 const btn = document.getElementById("openModal");
 const span = document.getElementById("closeModal");
 
-// Quando o botão é clicado, abre o modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// Quando o usuário clica no "x" (fechar), fecha o modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// Quando o usuário clica fora do modal, fecha o modal
+
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
