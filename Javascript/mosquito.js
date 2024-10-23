0e2aa4b3e48e849d6708e88bbbfadc2b97ca0923
GabriellyBonfim <gabriellybonfim03@gmail.com>
2024-10-23 10:39:40 -0300

    aaa
    aa

--- Javascript/mosquito.js
+++ Javascript/mosquito.js
@@ -1,24 +1,54 @@
-let altura = 0;
-let largura = 0;
+let altura, largura = 0;
 
-function ajustaPalco(){
-    altura = window.innerHeight;
-    largura = window.innerWidth;
-    console.log (largura,altura);
+function ajustaTela() {
+    altura = window.innerHeight
+    largura = window.innerWidth
+    console.log(largura, altura)
 }
 
-ajustaPalco();
+ajustaTela()
 
 function posRandomica(){
-let posx = Math.floor(Math.random() * largura)
-let posy = Math.floor(Math.random() * altura)
+let posx = Math.floor(Math.random() * largura) -90;
+let posy = Math.floor(Math.random() * altura) -90;
+
+posx = posx < 0 ? 0: posx;
+posy = posy < 0 ? 0: posy;
+
+console.log (posx,posy);
 
 let mosquito = document.createElement('img')
 mosquito.src= 'imagens/mosquito.png'
-mosquito.className = 'mosq01'
+mosquito.className = tamanhoMosquito() + ladoAleatorio ()
 mosquito.style.left = posx + 'px'
 mosquito.style.top = posy + 'px'
 mosquito.style.position = 'absolute'
 
 document.body.appendChild (mosquito)
+
+tamanhoMosquito()
+}
+
+function tamanhoMosquito(){
+    let classe = Math.floor (Math.random()* 3);
+   
+    switch (classe){
+        case 0:
+            return 'mosq01'
+        case 1:
+            return 'mosq02'
+        case 2:
+            return 'mosq03'
+    }
+}
+
+function ladoAleatorio (){
+    let classe = Math.floor (Math.random()* 2);
+   
+    switch (classe){
+        case 0:
+            return 'ladoA'
+        case 1:
+            return 'ladoB'
+    }
 }
